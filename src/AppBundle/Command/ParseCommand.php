51a4795ca1a7375d3f5b40f7c15e3dd1f7c05b60
trashkaloff <trashkaloff@gmail.com>
2017-04-12 17:16:23 +0300

    firstParsing

--- src/AppBundle/Command/ParseCommand.php
+++ src/AppBundle/Command/ParseCommand.php
@@ -1,6 +1,8 @@
 <?php
 namespace AppBundle\Command;
 
+use AppBundle\Entity\ClassSymfony;
+use AppBundle\Entity\NamespaceSymfony;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -11,23 +13,33 @@ class ParseCommand extends Command
     protected function configure()
     {
         $this
-            // the name of the command (the part after "bin/console")
             ->setName('app:parse')
-
-            // the short description shown while running "php bin/console list"
             ->setDescription('Parse api.symfony.com')
-
-            // the full command description shown when running the command with
-            // the "--help" option
             ->setHelp('This command allows you to create a user...');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $html = file_get_contents('http://api.symfony.com/3.2/');
-        $crowler = new Crawler($html);
+        $html = file_get_contents('http://api.symfony.com/3.2/namespaces.html');
+        $crawler = new Crawler($html);
 
-        foreach ($crowler->filter('a') as $item){
+        foreach ($crawler->filter('div.namespaces') as $item){
+            $url = 'http://api.symfony.com/3.2/' .$item->getAttribute('href');
+            $nameSpaces = new NamespaceSymfony();
+            $nameSpaces->setName($item->textContent);
+            $nameSpaces->setUrl($url);
+
+            $target = file_get_contents($url);
+            $DOMcrawler = new Crawler($target);
+
+            foreach ($DOMcrawler->filter('p') as $itemes){
+                    $itemes->getAttribute('href');
+                    $class = new ClassSymfony();
+                    $class->setUrl($itemes);
+                    $class->setName($itemes->textContent);
+                    $class->setNamespace($nameSpaces);
+             }
+
             var_dump($item->nodeValue);
         }
     }
